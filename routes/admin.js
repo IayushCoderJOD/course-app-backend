353cee7dc8688603039f1b5377d83ebe5e0f36e0
ayush tyagi <ayushtyagi4810poco@gmail.com>
2024-03-11 16:56:35 +0530

    backend of course app

---
+++ routes/admin.js
@@ -0,0 +1,55 @@
+const { Router } = require("express");
+const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
+const router = Router();
+
+// Admin Routes
+router.post("/signup", (req, res) => {
+  // Implement admin signup logic
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //   check if the user with this username already exists.
+  //   Admin.findOne(username);
+
+  Admin.create({
+    username,
+    password,
+  });
+
+  res.json({
+    msg: "Admin created successfully",
+  });
+});
+
+router.post("/courses", adminMiddleware, async (req, res) => {
+  // Implement course creation logic
+  const title = req.body.title;
+  const description = req.body.description;
+  const imageLink = req.body.imageLink;
+  const price = req.body.price;
+
+  const newCourse = await Course.create({
+    title,
+    description,
+    imageLink,
+    price,
+  });
+
+  res.json({
+    msg: "Course created successfully",
+    courseId: newCourse._id,
+  });
+});
+
+router.get("/courses", adminMiddleware, async (req, res) => {
+  // Implement fetching all courses logic
+  const allCourse = await Course.find({});
+
+  res.json({
+    courses: allCourse,
+    length: allCourse.length,
+  });
+});
+
+module.exports = router;
